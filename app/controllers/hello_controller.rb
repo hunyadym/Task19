024370a545c718ee9f5d5a998b379a295df4c47b
esziger <esziger@gmail.com>
2013-09-19 17:24:02 +0200

    user name updated

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,5 +1,5 @@
 class HelloController < ApplicationController
-http_basic_authenticate_with :name => "user", :password => "jelszo"
+http_basic_authenticate_with :name => "admin", :password => "jelszo"
 def home
 end
 
